d46514c6356a80e7e7c2cd6569190d956e7ea6ac
tracedwolf <tcwolfe04@gmail.com>
2026-04-06 10:07:22 -0500

    Updated greeting message.

--- Wolfe_Lab1/Wolfe_Lab1.cpp
+++ Wolfe_Lab1/Wolfe_Lab1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello from Tyler! This is my first Git commit.\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
